48a4878d210e34876eb6175b7f64908ef2eb0c20
LumunCaleb <167688226+LumunCaleb@users.noreply.github.com>
2024-10-04 05:45:07 +0100

    Update pdf-audio.py

--- pdf-audio.py
+++ pdf-audio.py
@@ -82,7 +82,7 @@ def extract_pages(pdf_reader, page_selection):
 # Streamlit App Interface
 st.title("PDF to Audio Converter")
 st.markdown("A Project by **YUSUF ABDUL** - NACEST/COM/HND22/780")
-st.markdown("Departmet of Computer Science")
+st.markdown("<u>Departmet of Computer Science</u>")
 st.markdown("*Supervisor:* Mr. Ike Innocent")
 
 # Upload the PDF file
